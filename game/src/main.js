6c6e689c70e6256426cd1324d0b8bb2cae1e5e9b
JordanDestin <destin.jord@gmail.com>
2025-02-27 17:52:26 -0400

    delete folder

--- game/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-// Importer Bootstrap CSS et JS
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-createApp(App).mount('#app')
